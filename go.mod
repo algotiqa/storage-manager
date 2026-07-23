b54abd2f8dc9e9432157ee0db204395944e3dfac
acarboni <andrea.carboni71@gmail.com>
2026-07-23 19:44:48 +0200

    Added CI/CD build workflow

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/algotiqa/storage-manager
 go 1.25.8
 
 require (
-	github.com/algotiqa/core v1.20.11
+	github.com/algotiqa/core v1.21.1
 	github.com/gin-gonic/gin v1.12.0
 )
 
--- main.go
+++ main.go
@@ -24,13 +24,14 @@ import (
 //=============================================================================
 
 const component = "storage-manager"
+var   version   = "dev"
 
 //=============================================================================
 
 func main() {
 	cfg := &app.Config{}
 	boot.ReadConfig(component, cfg)
-	logger := boot.InitLogger(component, &cfg.Application)
+	logger := boot.InitLogger(component, version, &cfg.Application)
 	engine := boot.InitEngine(logger, &cfg.Application)
 	initClients()
 	backend.InitStorage(cfg)
